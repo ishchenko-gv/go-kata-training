3c8ca93b94bd9a86eab15989803c0acd3e29770a
Georgii Ishchenko <ishchenko.gv@gmail.com>
2026-05-11 20:34:33 +0100

    fix test

--- 03-graceful-shutdown-server/server_test.go
+++ 03-graceful-shutdown-server/server_test.go
@@ -19,6 +19,7 @@ func TestServer(t *testing.T) {
 	s := server.NewServer(workerPool, broker, server.WithServerPort(3001), server.WithSignalChan(signalCh))
 	go s.Start()
 
+	c := http.DefaultClient
 	req, err := http.NewRequest(http.MethodPost, "http://localhost:3001/schedule-job", nil)
 	assert.NoError(t, err)
 
@@ -27,27 +28,36 @@ func TestServer(t *testing.T) {
 		req, err := http.NewRequest(http.MethodGet, "http://localhost:3001/jobs-status", nil)
 		assert.NoError(t, err)
 
-		c := http.DefaultClient
 		resp, err := c.Do(req)
 		assert.NoError(t, err)
 		defer resp.Body.Close()
 
+		var interrupted bool
+		var eventsCount int
 		scanner := bufio.NewScanner(resp.Body)
 		for scanner.Scan() {
 			line := scanner.Text()
+			if line != "" {
+				eventsCount++
+			}
 			t.Log(line)
+			t.Log("eventsCount", eventsCount)
+			if eventsCount == 3 && !interrupted {
+				signalCh <- os.Interrupt
+				interrupted = true
+			}
+
+			if eventsCount == 4 {
+				assert.Contains(t, line, "event: shutdown")
+			}
 		}
 
 		jobsStatusCh <- struct{}{}
 	}()
 
-	c := http.DefaultClient
-	c.Do(req)
-	c.Do(req)
 	c.Do(req)
 	c.Do(req)
 	c.Do(req)
 
-	//signalCh <- os.Interrupt
 	<-jobsStatusCh
 }
